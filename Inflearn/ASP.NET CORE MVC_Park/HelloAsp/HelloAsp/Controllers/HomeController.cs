d0f21705224a0ae35a1fa5f4418d506f24be3fa2
Sup <svra0945@gmail.com>
2024-12-09 07:58:39 +0900

    Lecture1~2

---
+++ Inflearn/ASP.NET CORE MVC_Park/HelloAsp/HelloAsp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloAsp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(string userid)
+        {
+            return $"응답입니다.{userid}";
+        }
+    }
+}
