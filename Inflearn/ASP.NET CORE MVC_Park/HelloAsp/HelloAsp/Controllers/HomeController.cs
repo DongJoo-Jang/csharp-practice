5c29e5ad02fe9124238154402b210b44dba393da
Sup <svra0945@gmail.com>
2024-12-10 00:03:20 +0900

    .gitignore추가, To Lecture4

--- Inflearn/ASP.NET CORE MVC_Park/HelloAsp/HelloAsp/Controllers/HomeController.cs
+++ Inflearn/ASP.NET CORE MVC_Park/HelloAsp/HelloAsp/Controllers/HomeController.cs
@@ -4,9 +4,17 @@ namespace HelloAsp.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index(string userid)
+        public string Index()
         {
-            return $"응답입니다.{userid}";
+            string userid = Request.Query["userid"];
+            string age = Request.Query["age"];
+            return $"응답입니다.{userid}_{age}";
         }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+
     }
 }
